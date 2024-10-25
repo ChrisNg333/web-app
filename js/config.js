f5aec5f353a0cee8d192fb8b27afeea64fb1bcf0
ChrisNg333 <123434224+ChrisNg333@users.noreply.github.com>
2024-10-24 19:55:01 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_wyjbnpH1I', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '21o39gmqa2j66c0dme051fb102', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_nSIuFsAnl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1hlmriv4u4rk2haju9emqle4rm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
